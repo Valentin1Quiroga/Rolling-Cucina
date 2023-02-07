3b41577ec9ef9a3bddddb21da8d63bdf57befbd2
Valentin Quiroga <valegolquiroga@gmail.com>
2023-02-06 21:52:48 -0300

    afinando login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import CardGroup from "../components/CardGroup/CardGroup";
 import Header from "../components/Header/Header";
-
+import Navbar from "../components/Navbar/Nabvar"
 const Home = () => {
     return ( 
         <>
+        <Navbar/>
         <Header/>
         <CardGroup/>
         </>
