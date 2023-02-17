86a3d3e7208c3d28b52c91f107b8ca4744884601
Marcos Rigo <marcos.rigo.10@gmail.com>
2023-02-17 00:16:22 -0300

    cambios en el modal del producto

--- src/components/Card-menu/Card-menu.jsx
+++ src/components/Card-menu/Card-menu.jsx
@@ -1,5 +1,8 @@
 import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import BtnVerProducto from '../BtnVerProducto/BtnVerProducto';
+import FormLogin from '../FormLogin/FormLogin';
+import GeneralModal from '../GeneralModal/GeneralModal';
 import "./Card-menu.css"
 
 const CardMarcos = ({title, ingredientes, imagen, precio}) => {
@@ -7,13 +10,18 @@ const CardMarcos = ({title, ingredientes, imagen, precio}) => {
         
             <Card id="tarjeta-menu">
                 <div id='contenedor-img'>
-                    <Card.Img className='img-fluid w-100' src={imagen} id="img-menu" />
+                    <Card.Img className='img-fluid w-100' src={imagen} />
                 </div>
                 <div id='cuerpo-menu'>
                     <Card.Body>   
                         <div id='encabezado-menu'>
                             <Card.Title id='titulo-menu'>{title}</Card.Title>
-                            <Button id='boton-ver-menu'>Ver</Button>    
+                            <GeneralModal 
+                            buttonText="Ver"
+                            variant="primary"
+                            modalTitle={title}
+                            modalBody = {<BtnVerProducto imagen={imagen} title={title} ingredientes={ingredientes} precio={precio}/>}
+                            />
                         </div>                       
                         
                         <Card.Text>
