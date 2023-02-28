df3eb7f38b626bba4a9ce90b300b43049501b15f
Valentin Quiroga <valegolquiroga@gmail.com>
2023-02-28 14:21:32 -0300

    solving deploy problem

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,17 +1,15 @@
 import Footer from "../components/Footer/Footer";
 import Errorcontainer from "../components/Errorcontainer/Errorcontainer";
-import Navbar from "../components/Navbar/Nabvar"
-import BotonCarrito from "../components/BotonCarrito/BotonCarrito";
-
+import Navbar from "../components/Navbar/Nabvar";
 
 const Error404 = () => {
-    return ( 
-        <>
-        <Navbar/>
-        <Errorcontainer/>
-        <Footer/>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Navbar />
+      <Errorcontainer />
+      <Footer />
+    </>
+  );
+};
+
 export default Error404;
