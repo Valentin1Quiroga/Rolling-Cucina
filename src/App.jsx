ffe433184ea4356a6a34f858f2cf7c5a3b42f70e
Valentin Quiroga <valegolquiroga@gmail.com>
2023-02-28 14:27:15 -0300

    solving deploy problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import TestLuis from "./pages/TestLuis";
-import TestPage from "./pages/TestPage";
 import TestValentin from "./pages/TestValentin";
-import TestPage2 from "./pages/TestPage2";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
-import AdminRoute from "./routes/AdminRoute";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import TestJavier from "./pages/TestJavier";
