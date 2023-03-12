26901f299fa71970ea903fc88bf5bfe50afdd085
martin <martinfanlo31@gmail.com>
2023-03-12 18:37:17 -0300

    validations are availables

--- src/components/FormRegistro/FormRegistro.jsx
+++ src/components/FormRegistro/FormRegistro.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import axios from "../../config/axios";
 import emailjs from "@emailjs/browser";
 import { useNavigate } from "react-router-dom";
+import "./FormRegistro.css"
 const FormRegistro = ({ handleClose, getUsers }) => {
   const navigate = useNavigate();
   const [values, setValues] = useState({
@@ -92,24 +93,27 @@ const FormRegistro = ({ handleClose, getUsers }) => {
           onChange={handleChange}
           value={values.name}
           name="name"
-          pattern="[A-Za-z]{2,50}"
+          pattern="[A-Za-z ]{2,50}"
         />
       </Form.Group>
       <Form.Group className="mb-3" controlId="userPhone">
         <Form.Label>Numero de Telefono</Form.Label>
+        <Form.Text className= "text-muted d-flex fst-italic msg-psw"> Ingrese su número de teléfono sin el 0 y sin el 15.</Form.Text>
         <Form.Control
           required
           type="text"
-          placeholder="Ingrese su numero de telefono sin el 0 y sin el 15"
+          placeholder="XXXX-XXXXXX"
           onChange={handleChange}
           value={values.phone}
           name="phone"
           minLength={10}
           maxLength={10}
+          pattern="[0-9]{10}"
         />
       </Form.Group>
       <Form.Group className="mb-3" controlId="userEmail">
         <Form.Label>Correo Electronico</Form.Label>
+        <Form.Text className= "text-muted d-flex fst-italic msg-psw"> Este correo será el usuario con el cual ingresará a la página.</Form.Text>
         <Form.Control
           required
           type="email"
@@ -123,6 +127,7 @@ const FormRegistro = ({ handleClose, getUsers }) => {
       </Form.Group>
       <Form.Group className="mb-3" controlId="userPassword">
         <Form.Label>Contraseña</Form.Label>
+        <Form.Text className= "text-muted d-flex fst-italic msg-psw"> Debe contener como mínimo 8 caracteres con al menos 1 minúscula, 1 mayúscula y 1 número.</Form.Text>
         <Form.Control
           required
           type="password"
@@ -139,7 +144,7 @@ const FormRegistro = ({ handleClose, getUsers }) => {
         <Form.Control
           required
           type="password"
-          placeholder="Contraseña"
+          placeholder="Repita Contraseña"
           onChange={handleChange}
           value={values.repeat_password}
           name="repeat_password"
