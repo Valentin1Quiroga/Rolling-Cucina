35bce19994b3d0475c39d8b0852a3dde9e37ee9e
Valentin Quiroga <valegolquiroga@gmail.com>
2023-11-17 20:23:43 -0300

    agregue spinner cuando carga el inicio de sesion

--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -10,9 +10,11 @@ import axios from "../../config/axios";
 import { Alert } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import ForgotPasswordModal from "../Forgot-password-modal/Forgot-password-modal";
+import Spinner from "../Spinner/Spinner";
 
 const FormLogin = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [values, setValues] = useState({
     email: "",
     password: "",
@@ -27,6 +29,7 @@ const FormLogin = () => {
   const submitForm = async (e) => {
     try {
       e.preventDefault();
+      setLoading(true);
       const { data } = await axios.post("/users/login", values);
       localStorage.setItem("token", data.token);
       localStorage.setItem("id", data.user._id);
@@ -53,99 +56,104 @@ const FormLogin = () => {
       toast.error(
         "Ups! Error al iniciar sesion. Intenta de nuevo en unos minutos"
       );
+    } finally {
+      setLoading(false); // Establece loading a false después de que la llamada a la API se complete
     }
   };
 
   return (
-    <Form
-      className="border border-2 form-login p-5 rounded-5 w-75"
-      onSubmit={submitForm}
-    >
-      <Form.Group className="mt-4 mb-5" controlId="formBasicEmail">
-        <Form.Label className="label-login text-muted">Usuario</Form.Label>
-        <Form.Control
-          value={values.email}
-          onChange={handleChange}
-          name="email"
-          type="email"
-          className="bg-transparent text-center p-2"
-          placeholder="USUARIO"
-          required
-          minLength={5}
-          maxLength={40}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label className="label-login text-muted">Contraseña</Form.Label>
-        <Form.Control
-          value={values.password}
-          onChange={handleChange}
-          name="password"
-          type="password"
-          className="bg-transparent text-center p-2"
-          placeholder="CONTRASEÑA"
-          required
-          minLength={6}
-          maxLength={20}
-        />
-        <GeneralModal
-          claseBoton="border-0 bg-transparent text-muted"
-          buttonText="Olvidaste tu contraseña"
-          modalTitle="Recuperar contraseña"
-          boton={true}
-          modalBody={<ForgotPasswordModal />}
-        />
-      </Form.Group>
-      <div className="list-unstyled d-flex justify-content-sm-between justify-content-center flex-wrap">
-        <div className="d-flex flex-column">
-          <Form.Text className="text-muted d-flex justify-content-end pb-1  ">
-            Aun no tienes cuenta?
-          </Form.Text>
-          <GeneralModal
-            boton={true}
-            buttonText="Registrate"
-            variant="transparent"
-            modalTitle="Crea tu cuenta"
-            modalBody={<FormRegistro />}
+    <>
+      <Form
+        className="border border-2 form-login p-5 rounded-5 w-75"
+        onSubmit={submitForm}
+      >
+        <Form.Group className="mt-4 mb-5" controlId="formBasicEmail">
+          <Form.Label className="label-login text-muted">Usuario</Form.Label>
+          <Form.Control
+            value={values.email}
+            onChange={handleChange}
+            name="email"
+            type="email"
+            className="bg-transparent text-center p-2"
+            placeholder="USUARIO"
+            required
+            minLength={5}
+            maxLength={40}
           />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label className="label-login text-muted">Contraseña</Form.Label>
+          <Form.Control
+            value={values.password}
+            onChange={handleChange}
+            name="password"
+            type="password"
+            className="bg-transparent text-center p-2"
+            placeholder="CONTRASEÑA"
+            required
+            minLength={6}
+            maxLength={20}
+          />
+          <GeneralModal
+            claseBoton="border-0 bg-transparent text-muted"
+            buttonText="Olvidaste tu contraseña"
+            modalTitle="Recuperar contraseña"
+            boton={true}
+            modalBody={<ForgotPasswordModal />}
+          />
+        </Form.Group>
+        <div className="list-unstyled d-flex justify-content-sm-between justify-content-center flex-wrap">
+          <div className="d-flex flex-column">
+            <Form.Text className="text-muted d-flex justify-content-end pb-1  ">
+              Aun no tienes cuenta?
+            </Form.Text>
+            <GeneralModal
+              boton={true}
+              buttonText="Registrate"
+              variant="transparent"
+              modalTitle="Crea tu cuenta"
+              modalBody={<FormRegistro />}
+            />
+          </div>
+          <Button variant="success" type="submit">
+            Iniciar sesion <SlLogin />
+          </Button>
         </div>
-        <Button variant="success" type="submit">
-          Iniciar sesion <SlLogin />
-        </Button>
-      </div>
-      <div>
-        <ul className="list-unstyled d-flex justify-content-center mt-5">
-          <li>
-            <a
-              href="https://www.facebook.com/profile.php?id=100090240022071"
-              target="_blank"
-              className="text-dark"
-            >
-              <i className="fa-brands fa-facebook me-2 fa-2x"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/rolling.cucina/"
-              target="_blank"
-              className="text-dark"
-            >
-              <i className="fa-brands fa-instagram mx-2 fa-2x"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://twitter.com/CucinaRolling"
-              target="_blank"
-              className="text-dark"
-            >
-              <i className="fa-brands fa-twitter mx-2 fa-2x"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-      {backError && <Alert variant="danger"> {backError}</Alert>}
-    </Form>
+        <div>
+          <ul className="list-unstyled d-flex justify-content-center mt-5">
+            <li>
+              <a
+                href="https://www.facebook.com/profile.php?id=100090240022071"
+                target="_blank"
+                className="text-dark"
+              >
+                <i className="fa-brands fa-facebook me-2 fa-2x"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/rolling.cucina/"
+                target="_blank"
+                className="text-dark"
+              >
+                <i className="fa-brands fa-instagram mx-2 fa-2x"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/CucinaRolling"
+                target="_blank"
+                className="text-dark"
+              >
+                <i className="fa-brands fa-twitter mx-2 fa-2x"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        {backError && <Alert variant="danger"> {backError}</Alert>}
+      </Form>
+      {loading && <Spinner />}
+    </>
   );
 };
 
